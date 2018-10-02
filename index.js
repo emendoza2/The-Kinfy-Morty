01dd43254b0743fe2142ec1496c9d6bca4e6ab66
123PepitheKing <joseantolinbenitez@gmail.com>
2018-10-02 22:34:35 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'marco') {
+    msg.reply('polo')
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
