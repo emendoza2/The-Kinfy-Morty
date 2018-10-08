acbcc98ce4c783e3e0a42c426de75c837319d439
123PepitheKing <joseantolinbenitez@gmail.com>
2018-10-08 10:19:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.on('message', msg => {
 client.on('message', msg => {
   if (msg.content === 'Kinfy start') {
     msg.reply('Congratulations, you recieved a Regular Morty!')
-    <img src="PM-001.png"/> 
+    <img src='PM-001.png'/> 
     }
 });
 
